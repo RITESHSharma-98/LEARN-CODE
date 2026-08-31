5efa23c1993786b78f0a4248c521c614fcae736b
Ritesh Sharma <ritesh123.cse@gmail.com>
2026-08-31 16:15:26 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+package java_repo;
+class Main {
+    public static void main(String[] args) {
+                
+        System.out.println("Hello World");
+
+    }
+}
+// make api for create
+class Create {
+    String customerFirstName;
+    String customerLastName;
+    String custumerIssue;
+    String custumerNumber;
+    String custumerEmail;
+    String custunerAttchment;
+
+}
+
